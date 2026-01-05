c32eb1f55ff845a6daf5c66ebf52720c55515488
Rajeev Kumar <rajeevkumarrj7@gmail.com>
2026-01-05 14:34:59 +0530

    added edit user info

--- src/components/header/CompleteProfile.jsx
+++ src/components/header/CompleteProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./CompleteProfile.css";
 
 const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
@@ -7,6 +7,48 @@ const CompleteProfile = () => {
   const nameRef = useRef();
   const imageUrlRef = useRef();
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const idToken = localStorage.getItem("token");
+      if (!idToken){
+        console.error("No idToken found!");
+        return;
+      } 
+
+      try {
+        const response = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_KEY}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ idToken }),
+          }
+        );
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error.message);
+        }
+
+        const user = data.users[0];
+
+       
+        if (user.displayName) {
+          nameRef.current.value = user.displayName;
+        }
+
+        if (user.photoUrl) {
+          imageUrlRef.current.value = user.photoUrl;
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
+    fetchProfile();
+  }, []);
 
   const updateProfileHandler = async (e) => {
     e.preventDefault();
@@ -35,7 +77,7 @@ const CompleteProfile = () => {
         throw new Error(data.error.message);
       }
 
-      alert("Profile updated successfully..");
+      alert("Profile updated successfully");
     } catch (error) {
       alert(error.message);
     }
@@ -49,22 +91,12 @@ const CompleteProfile = () => {
         <h2>Complete Your Profile</h2>
 
         <div className="profile-field">
-          <input
-            type="text"
-            ref={nameRef}
-            required
-            placeholder=" "
-          />
+          <input type="text" ref={nameRef} required placeholder=" " />
           <label>Full Name</label>
         </div>
 
         <div className="profile-field">
-          <input
-            type="url"
-            ref={imageUrlRef}
-            required
-            placeholder=" "
-          />
+          <input type="url" ref={imageUrlRef} required placeholder=" " />
           <label>Profile Photo URL</label>
         </div>
 
